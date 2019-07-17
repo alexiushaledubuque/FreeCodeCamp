5449ff4c3c19a1b2e4dc4710e5ceeb371439ae6e
Cindy Alexius Hale-Dubuque <alexiushaledubuque@gmail.com>
2019-07-16 21:25:50 -0400

    Added FCP solutions and refactored my solution

--- Algorithms/Intermediate/Sum All Numbers in a Range.js
+++ Algorithms/Intermediate/Sum All Numbers in a Range.js
@@ -31,3 +31,26 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]);
+
+// Intermediate FCP Solution
+function sumAll(arr) {
+  // Buckle up everything to one!
+
+  // Using ES6 arrow function (one-liner)
+  var sortedArr = arr.sort((a,b) => a-b);
+  var firstNum = arr[0];
+  var lastNum = arr[1];
+  // Using Arithmetic Progression summing formula
+
+  var sum = (lastNum - firstNum + 1) * (firstNum + lastNum) / 2;
+  return sum;
+}
+
+// Advanced FCP Solution
+function sumAll(arr) {
+    var sum = 0;
+    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+        sum += i;
+    }
+  return sum;
+}
