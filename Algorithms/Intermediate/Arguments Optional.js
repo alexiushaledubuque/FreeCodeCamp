fbd2337005ee2e1e1c1441b3dbab4d4b8a2fe156
Cindy Alexius Hale-Dubuque <alexiushaledubuque@gmail.com>
2020-05-03 15:38:07 -0400

    Working with Arguments

--- Algorithms/Intermediate/Arguments Optional.js
+++ Algorithms/Intermediate/Arguments Optional.js
@@ -1,8 +1,77 @@
-function steamrollArray(arr, flatArr = []) {
-  const elem = arr.pop();
-  return elem
-    ? !Array.isArray(elem)
-      ? steamrollArray(arr, [elem, ...flatArr])
-      : steamrollArray(arr.concat(elem), flatArr)
-    : flatArr;
+function addTogether(a) {
+  if (arguments.length > 1) {
+    return [...arguments].reduce((accumulator, value) => {
+      return typeof value == "number" ? accumulator += value : undefined
+    })
+  } else {
+    if (typeof arguments[0] == 'string') return undefined
+    return (b) => {
+      return typeof b == "number" ? a + b : undefined
+    }
+  }
+}
+addTogether(2,3);
+
+// FCC Basic Solution
+function addTogether() {
+  // Function to check if a number is actually a number
+  // and return undefined otherwise.
+  var checkNum = function(num) {
+    if (typeof num !== "number") {
+      return undefined;
+    } else return num;
+  };
+
+  // Check if we have two parameters, check if they are numbers
+  // handle the case where one is not
+  // returns the addition.
+  if (arguments.length > 1) {
+    var a = checkNum(arguments[0]);
+    var b = checkNum(arguments[1]);
+    if (a === undefined || b === undefined) {
+      return undefined;
+    } else {
+      return a + b;
+    }
+  } else {
+    // If only one parameter was found, returns a new function that expects two
+    // Store first argument before entering the new function scope
+    var c = arguments[0];
+
+    // Check the number again, must be outside the function to about returning an object
+    // instead of undefined.
+    if (checkNum(c)) {
+      // Return function that expect a second argument.
+      return function(arg2) {
+        // Check for non-numbers
+        if (c === undefined || checkNum(arg2) === undefined) {
+          return undefined;
+        } else {
+          // if numbers then add them.
+          return c + arg2;
+        }
+      };
+    }
+  }
+}
+
+// FCC Intermediate Solution
+function addTogether(first, second) {
+  if (typeof first !== "number") {
+    return undefined;
+  }
+  const sum = second =>
+    typeof second === "number" ? first + second : undefined;
+  return typeof second === "undefined" ? second => sum(second) : sum(second);
+}
+
+//FCC Advanced Solution
+//jshint esversion: 6
+function addTogether() {
+  var args = Array.from(arguments);
+  return args.some(n => typeof n !== "number")
+    ? undefined
+    : args.length > 1
+    ? args.reduce((acc, n) => (acc += n), 0)
+    : n => (typeof n === "number" ? n + args[0] : undefined);
 }
