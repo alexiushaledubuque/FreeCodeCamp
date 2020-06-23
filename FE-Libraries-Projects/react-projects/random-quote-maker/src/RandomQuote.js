fe1d59e9ceee748a46e93898cbcba7ee53832525
Cindy Alexius Hale-Dubuque <alexiushaledubuque@gmail.com>
2020-06-22 22:29:32 -0400

    Updated styles

--- FE-Libraries-Projects/react-projects/random-quote-maker/src/RandomQuote.js
+++ FE-Libraries-Projects/react-projects/random-quote-maker/src/RandomQuote.js
@@ -4,24 +4,46 @@ import quotes from "./data/quotes.js"
 class RandomQuote extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick.bind(this)
+    this.state = {
+      quote: '',
+      author: '',
+      backgroundColor: ''
+    }
+    this.getNewQuote = this.getNewQuote.bind(this)
   }
   
-  handleClick = () => {
-    const quoteMath = Math.floor(Math.random() * 102) + 1 
-    const nextQuote = quotes[quoteMath]
-    return nextQuote;
+  getNewQuote = () => {
+    const quoteMath = Math.floor(Math.random() * 102) + 1,
+          nextQuote = quotes[quoteMath],
+          nextColor = '#'+Math.random().toString(16).substr(-6);
+          
+    this.setState({
+      quote: nextQuote.quote,
+      author: nextQuote.author,
+      backgroundColor: nextColor
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      quote: quotes[0].quote,
+      author: quotes[0].author})
   }
   render() {
-    const quoteMath = Math.floor(Math.random() * 102) + 1 
-    const nextQuote = quotes[quoteMath]
     return (
-      <div className="new-quote">
-          {quoteMath}
-          {nextQuote.quote}
-          {nextQuote.author}
-      
-      {/* <button onClick={this.handleClick}>New quote</button> */}
+      <div id="quote-box">
+        <div id="text" style={{backgroundColor: this.state.backgroundColor}}>
+          {this.state.quote} <br/><br/>
+        </div>
+        <div id="author" style={{backgroundColor: this.state.backgroundColor}}>
+          By: {this.state.author}<br/><br/>
+        </div>
+        <div id="new-quote">
+          <button id="twitter">Twitter</button>
+          <button id="facebook">Facebook</button>
+          <button id="quote-btn" onClick={this.getNewQuote}>New quote</button>
+        </div>
+        <a id="tweet-quote"></a>
       </div>
     )
   };
